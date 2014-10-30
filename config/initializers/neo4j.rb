f10faeb10c47b74742b2bd291ee1b7bb59d04553
Daniel J. Pritchett <dpritchett@gmail.com>
2014-10-30 12:04:02 -0500

    Added neo4j env var

--- config/initializers/neo4j.rb
+++ config/initializers/neo4j.rb
@@ -1,1 +1,7 @@
-Neo4j::Session.open(:server_db)
+neo4j_url = ENV.fetch('NEO4J_URI', 'http://localhost:7474')
+
+uri        = URI.parse(neo4j_url)
+
+server_uri = "#{uri.scheme}://#{uri.host}:#{uri.port}"
+
+Neo4j::Session.open(:server_db, server_uri, basic_auth: { username: uri.user, password: uri.password})
