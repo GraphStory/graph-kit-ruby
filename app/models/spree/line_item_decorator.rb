3bc9b8c08a5eac56179cd61aefd897b76b09e369
Daniel J. Pritchett <dpritchett@gmail.com>
2014-11-04 20:56:14 -0600

    Don't log line items for not-logged-in-users

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -4,6 +4,7 @@ Spree::LineItem.class_eval do
   delegate :user, to: :order, prefix: false
 
   def log_to_graph
+    return unless user.try(:graphed)
     user.graphed.purchases << product.graphed
   end
 end
