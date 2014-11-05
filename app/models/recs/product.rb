abc9822197282866ac30b86640ab256e42c525c8
Daniel J. Pritchett <dpritchett@gmail.com>
2014-11-04 21:37:28 -0600

    Basic cypher query working

--- app/models/recs/product.rb
+++ app/models/recs/product.rb
@@ -11,6 +11,13 @@ module Recs
       Spree::Product.find_by!(slug: slug)
     end
 
+    def users_also_bought
+      query_as(:product).
+        match("product<--(user:`Recs::User`)-->(other_product:`Recs::Product`)").
+        where('other_product.slug <> product.slug').
+        pluck('DISTINCT other_product')
+    end
+
     def self.from_spree_product(spree_product)
       where(slug: spree_product.slug).first ||
         create(name: spree_product.name, slug: spree_product.slug)
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,7 +1,6 @@
 Spree::Product.class_eval do
   def users_also_bought
-    users = line_items.flat_map(&:order).uniq.flat_map(&:user).uniq
-    users.flat_map(&:orders).uniq.flat_map(&:products).uniq - [self]
+    graphed.users_also_bought.map(&:in_spree)
   end
 
   def graphed
